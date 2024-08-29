bd0655b8b86c64b7ad7efce9d984e327deadaa6e
EricNorrwing <Eric.Norrwing@gmail.com>
2024-08-29 09:54:54 +0200

    first commit

---
+++ src/test/java/com/ericnorrwing/wsj_lek_5/WsjLek5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericnorrwing.wsj_lek_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsjLek5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
